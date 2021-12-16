4a86b4bd2c2763ddb3b8eefa53d3a79eeeb7465f
Luke Liu <Luke-Liu2020@qq.com>
2021-12-16 17:19:12 +0800

    new

---
+++ ui/try.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'try.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+import  resources.icons_rc
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        MainWindow.setStyleSheet("")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(210, 210, 251, 81))
+        self.pushButton.setIconSize(QtCore.QSize(30, 3))
+        self.pushButton.setCheckable(False)
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        self.pushButton.clicked.connect(MainWindow.close)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Close"))
+
+
+import sys
+# 主方法，程序从此处启动PyQt设计的窗体
+if __name__ == '__main__':
+   app = QtWidgets.QApplication(sys.argv)
+   app.setStyle("Fusion") # 设置窗口风格
+   MainWindow = QtWidgets.QMainWindow() # 创建窗体对象
+   ui = Ui_MainWindow() # 创建PyQt设计的窗体对象
+   ui.setupUi(MainWindow) # 调用PyQt窗体的方法对窗体对象进行初始化设置
+   MainWindow.show() # 显示窗体
+   sys.exit(app.exec_()) # 程序关闭时退出进程
