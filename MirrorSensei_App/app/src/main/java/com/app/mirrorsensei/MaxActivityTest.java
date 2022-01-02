6cb83ba65ec615e1dc679a4f0f592e37bbec950f
max12345 <max98tkc@gmail.com>
2022-01-03 00:46:55 +0800

    added File and Pref related stuff

--- MirrorSensei_App/app/src/main/java/com/app/mirrorsensei/MaxActivityTest.java
+++ MirrorSensei_App/app/src/main/java/com/app/mirrorsensei/MaxActivityTest.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 import com.app.mirrorsensei.UtilMax.utilmax;
 
+import java.io.File;
+
 public class MaxActivityTest extends AppCompatActivity {
 
     @Override
@@ -15,5 +17,30 @@ public class MaxActivityTest extends AppCompatActivity {
 
         //util init
         utilmax.setCurrActivity(this);
+
+        //set button onClick
+        findViewById(R.id.maxtestactivity_button1).setOnClickListener(v -> {
+//            testFileIO();
+//            createDeleteCache();
+//            utilmax.prefTemplate();
+        });
+    }
+
+    //note: write will overwrite existing content
+    private void testFileIO(){
+        File file = utilmax.makeFile("test");
+        utilmax.log(file.getAbsolutePath());
+        utilmax.createFile(file);
+        utilmax.writeFileTemplate(file,"qwerqwerqwer");
+        utilmax.writeFileTemplate(file,"asdfasdf");
+        utilmax.writeFileTemplate(file,"zxcv");
+        utilmax.writeFileTemplate(file,"qwer\nasdf\nzxcv");
+        utilmax.log(utilmax.readFileTemplate(file));
+    }
+
+    private void createDeleteCache(){
+        File cache = utilmax.createCacheFile("test");
+        utilmax.log(cache.getAbsolutePath());
+        utilmax.log(utilmax.deleteCacheFile(cache));
     }
 }
