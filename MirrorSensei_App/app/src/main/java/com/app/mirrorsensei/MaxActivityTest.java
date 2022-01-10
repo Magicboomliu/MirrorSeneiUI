449927e2f04e352468a041b95b172293f25cd26d
Foxriee <nabeela.k.5658@gmail.com>
2022-01-10 13:07:33 -0800

    hiiii

--- MirrorSensei_App/app/src/main/java/com/app/mirrorsensei/MaxActivityTest.java
+++ MirrorSensei_App/app/src/main/java/com/app/mirrorsensei/MaxActivityTest.java
@@ -31,7 +31,7 @@ public class MaxActivityTest extends AppCompatActivity {
 //            testCreateFile();
 //            testSharing();
 //            Arrays.stream(utilmax.listFile()).forEach(s -> utilmax.log(s));
-//            testAC();
+//            testAC(); Maxyyyyyy
             testJSON();
 //            testClass();
         });
