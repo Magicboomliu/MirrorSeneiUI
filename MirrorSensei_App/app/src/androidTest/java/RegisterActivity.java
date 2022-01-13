4ff1e1ca1bd2038dba4ceb3e291abdc8fcf1579a
Foxriee <nabeela.k.5658@gmail.com>
2022-01-12 21:24:39 -0800

    Login And Registation

---
+++ MirrorSensei_App/app/src/androidTest/java/RegisterActivity.java
@@ -0,0 +1,24 @@
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        TextView btn=findViewById(R.id.alreadyHaveAccount);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegisterActivity.this, LoginActivity.class));
+
+            }
+        });
+    }
+}
