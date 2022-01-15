0fc010d0e6ae5325f938e2563a8d010e7606a609
max12345 <max98tkc@gmail.com>
2022-01-15 09:55:24 +0800

    some more codes for buttons

--- MirrorSensei_App/app/src/main/java/com/app/mirrorsensei/MainFrontPage.java
+++ MirrorSensei_App/app/src/main/java/com/app/mirrorsensei/MainFrontPage.java
@@ -2,6 +2,7 @@ package com.app.mirrorsensei;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.app.mirrorsensei.UtilMax.utilmax;
@@ -25,9 +26,37 @@ public class MainFrontPage extends AppCompatActivity {
         utilmax.INIT();
         utilmaxMS.INIT_MS();
 
-        //set button onclick
-        binding.ARFeedbackMainscreen.setOnClickListener(view -> {
-            utilmax.log("it works");
+        //set button onClick
+        binding.currUser.setOnClickListener(view -> {
+            if (utilmaxMS.currUser==null){
+                startActivity(new Intent(utilmax.CURR_CONTEXT,LoginActivity.class));
+            } else {
+                utilmaxMS.accountSignOut();
+            }
         });
+        binding.socialshareMainscreen.setOnClickListener(view -> {
+            if (utilmaxMS.currUser==null){
+                utilmax.makeToast("Please sign in first");
+                return;
+            }
+            Intent intent = utilmax.getSharingIntent();
+            intent.putExtra(Intent.EXTRA_TEXT, "I am currently practicing "+ utilmaxMS.loadUserFile().subjectCategory+". Join me at Mirror Sensei!");
+            intent.setType("text/plain");
+            utilmax.startSharingSharesheet(intent,"Share current learning topic to:");
+        });
+        binding.tutorialMainscreen.setOnClickListener(view -> {
+            utilmax.log("for starting new course");
+        });
+        //set button onClick for buttons that won't be used
+        binding.ARFeedbackMainscreen.setOnClickListener(view -> utilmax.makeToast("Intended for AR feedback. Not implemented right now."));
+        binding.motionrecordingMainscreen.setOnClickListener(view -> utilmax.makeToast("Intended for Motion Recording related functions. Not implemented right now."));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (utilmaxMS.currUser!=null){
+            binding.currUser.setText(utilmaxMS.currUser);
+        }
     }
 }
